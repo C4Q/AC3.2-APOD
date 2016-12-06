9c38fa1a04cfed761622f534f0807b7f739733ac
Annie <annie.mmt@gmail.com>
2016-12-06 11:41:27 -0500

    Finished

--- NASA APOD/NASA APOD/NASA.swift
+++ NASA APOD/NASA APOD/NASA.swift
@@ -12,6 +12,12 @@ enum handleParseData: Error {
     case results, jsonSerialization
 }
 
+//enum APODError: String, Error {
+//    case image = "Missing image"
+//    case noType = "No type information"
+//    case unsupportedType = "Unsupported type"
+//}
+
 class Nasa {
     let date: String
     let explanation: String
@@ -27,23 +33,20 @@ class Nasa {
         self.mediaType = mediaType
     }
     
-    static func generateNasa(data: Data?) -> [Nasa]? {
-        var nasaToReturn = [Nasa]()
+    static func generateNasa(data: Data?) -> Nasa? {
         
         do {
             let jsonData: Any = try JSONSerialization.jsonObject(with: data!, options: [])
             guard let response = jsonData as? [String:AnyObject] else { throw handleParseData.jsonSerialization }
-
                 guard let date = response["date"] as? String,
                     let explanation = response["explanation"] as? String,
                     let title = response["title"] as? String,
                     let url = response["url"] as? String,
                     let mediaType = response["media_type"] as? String else { throw handleParseData.results }
-            
-            nasaToReturn.append(Nasa(date: date, explanation: explanation, title: title, url: url, mediaType: mediaType))
+            return Nasa(date: date, explanation: explanation, title: title, url: url, mediaType: mediaType)
         } catch {
             print("Unknown parsing error at \(error)")
         }
-        return nasaToReturn
+        return nil
     }
 }
--- NASA APOD/NASA APOD/NasaViewController.swift
+++ NASA APOD/NASA APOD/NasaViewController.swift
@@ -8,54 +8,75 @@
 
 import UIKit
 
-class NasaViewController: UIViewController {
+class NasaViewController: UIViewController, UIPickerViewDelegate {
     
-    var nasa: [Nasa]?
-    var apiPoint = "https://api.nasa.gov/planetary/apod?api_key=HHy5z1GsmcW1e7ODO6YLHFQxg2xuMCkzkOC23B67"
+    var nasa: Nasa?
+    var baseApiPoint = "https://api.nasa.gov/planetary/apod?api_key=HHy5z1GsmcW1e7ODO6YLHFQxg2xuMCkzkOC23B67"
     var customDate = "&date=YYYY-MM-DD"
     
-    
-// MARK: - Outlets
+    // MARK: - Outlets
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
     
-// MARK: - Actions
-    @IBAction func valueChanged(_ sender: UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        var StringDate = dateFormatter.string(from: sender.date)
-        dateFormatter.dateFormat = "YYYY-MM-DD"
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.label.text = nasa.description
-        loadData()
+        self.datePicker.maximumDate = Date()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGuesture))
+        image.isUserInteractionEnabled = true
+        image.addGestureRecognizer(tapGesture)
+     }
+    
+    var isVideo = false
+    var videoURL = ""
+    func handleTapGuesture() {
+        if isVideo {
+            if let url = URL(string: videoURL) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
     }
-
-
-// MARK: - Methods
-    func loadData() {
-        APIRequestManager.manager.getData(APIEndPoint: apiPoint) { (data: Data?) in
+    
+    // MARK: - Actions
+    @IBAction func valueChanged(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let stringDate = dateFormatter.string(from: sender.date)
+        loadData(date: "&date=\(stringDate)")
+    }
+    
+    // MARK: - Methods
+    func loadData(date: String) {
+        APIRequestManager.manager.getData(APIEndPoint: baseApiPoint + date) { (data: Data?) in
             guard let validData = data else { return }
             if let validNasa = Nasa.generateNasa(data: validData) {
-                self.nasa = validNasa
-                dump(validNasa)
-            }
-            if self.nasa?.mediaType == "image" {
-                APIRequestManager.manager.getData(APIEndPoint: apiPoint) {(data: Data?) in
-                    guard let validData = data else { return }
-                    if let validImage = UIImage(validData) {
-                        DispatchQueue.main.async {
-                            self.image = validImage
+                DispatchQueue.main.async {
+                    self.nasa = validNasa
+                    dump(validNasa)
+                    
+                    if let validatedNASA = self.nasa {
+                        if validatedNASA.mediaType == "image" {
+                            self.isVideo = false
+                            APIRequestManager.manager.getData(APIEndPoint: validatedNASA.url, callback: { (data: Data?) in
+                                guard let validData = data else { return }
+                                DispatchQueue.main.async {
+                                    self.image.image = UIImage(data: validData)
+//                                    self.image.layoutIfNeeded()
+                                }
+                            })
+                        } else {
+                            self.isVideo = true
+                            self.videoURL = validatedNASA.url
                         }
                     }
                 }
             }
-            else {
-                return
-            }
         }
     }
-
 }
+
+
+
+
+
